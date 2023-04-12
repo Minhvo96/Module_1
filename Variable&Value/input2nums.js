c22830494a10e81d1c1b191a87b5a92f745c5985
Minhvo96 <voquangminhbck38@gmail.com>
2023-04-12 11:15:30 +0700

    update

---
+++ Variable&Value/input2nums.js
@@ -0,0 +1,8 @@
+let a = prompt('Input number a:');
+let b = prompt('Input number b:');
+
+if(a % b == 0) {
+    alert(a+' is a multiple of '+b);
+} else {
+    alert(a+' is not a multiple of '+b);
+}
---
+++ Variable&Value/retangleacreage.js
@@ -0,0 +1,4 @@
+let width = 40;
+let height = 80;
+let acreage = width*height;
+document.write('Acreage = '+acreage)
