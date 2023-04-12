e0b0824e46d804583e5cf893d7bf641b61067089
Minhvo96 <voquangminhbck38@gmail.com>
2023-04-12 11:17:17 +0700

    update

---
+++ JavaScript/OnOffBulb/bulb2.js
@@ -0,0 +1,16 @@
+function light() {
+    document.getElementById('dentat').src='pic_bulbon.gif'
+}
+
+function bulb() {
+    document.getElementById('dentat').src='pic_bulbon.gif' ;
+    
+}
+
+function bulb1() {
+    document.getElementById('dentat').src='pic_bulboff.gif' ;
+}
+
+function light1() {
+    document.getElementById('dentat').src='pic_bulboff.gif'
+}
