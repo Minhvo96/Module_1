5a4ce0f2f00a443ebae795ba0232b82199a3d9e8
Minhvo96 <voquangminhbck38@gmail.com>
2023-04-19 14:00:19 +0700

    update

---
+++ Loop/Loop in Loop/multiplicationTable.js
@@ -0,0 +1,28 @@
+/*let table = "<table border='1' width='300' cellspacing='0' cellpadding='3'>";
+
+for (i = 1; i <= 10; i++) {
+    table += "<tr>";
+    for (j = 1; j <= 10; j++) {
+        table += "<td>" + i * j + "</td>";
+    }
+    table += "</tr>";
+}
+table += "</table>";
+document.write(table); */
+
+
+let table = "<table border '1' width '300px' cellspacing='0' cellpadding='3'>";
+
+for(i = 1; i <= 10; i++) {
+    table += "<tr>";
+    for(j = 1; j <= 10; j++) {
+        table += "<td>" + i*j + "</td>";
+    }
+}   table += "</tr>";
+table += "</table>";
+document.write(table);
+
+
+
+
+
